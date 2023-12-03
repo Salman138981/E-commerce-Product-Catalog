90cf1a2630fcaef3a26185a7fcd270d07393114e
salman <sk752547@gmail.com>
2023-12-04 03:49:53 +0530

    E-commerce application completed

---
+++ E-commerce-Product-Catalog-Application/src/main/java/com/masai/model/Category.java
@@ -0,0 +1,40 @@
+package com.masai.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="categories")
+public class Category {
+    
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Integer catId;
+
+	    @NotBlank(message = "Category Name is required")
+	    private String categoryName;
+	    
+	    @NotBlank(message = "Description is required")
+	    private String description;
+
+
+	    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+	    private List<Product> products;
+	    
+	    
+	    
+}
---
+++ E-commerce-Product-Catalog-Application/src/main/java/com/masai/model/Product.java
@@ -0,0 +1,53 @@
+package com.masai.model;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="products")
+public class Product {
+	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String productName;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
+    private Double price;
+    
+    @Positive(message = "Rating must be positive")
+    private Double rating;
+
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    
+    
+    
+    
+    
+	
+   
+}
